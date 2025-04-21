44fc9ac735bf1753d162658ba438de400e83bc71
Soyon <sochoi25@amherst.edu>
2025-04-21 19:39:41 -0400

    two different solver classes -- needs fixes

--- random_remove.py
+++ random_remove.py
@@ -1,8 +1,10 @@
 import os
 import random
 from pysmt.smtlib.parser import SmtLibParser
-from pysmt.smtlib.script import SmtLibCommand, SmtLibScript
-from pysmt.shortcuts import reset_env
+from pysmt.smtlib.script import SmtLibCommand, SmtLibScript, evaluate_command
+from pysmt.smtlib.solver import SmtLibSolver
+from pysmt.shortcuts import *
+from pysmt.typing import *
 import argparse
 import subprocess
 
@@ -65,13 +67,23 @@ def validate_with_z3(file_path):
     Validates the SmtLibScript found in the provided file with a Z3 solver.
     :param file_path: Path to SmtLibScript to verify.
     """
+    script = load_smt2_script(file_path)
+    with SmtLibSolver("z3") as solver:
+        cmds = script.commands
+        for cmd in cmds:
+            evaluate_command(cmd, solver)
 
-    try:
-        result = subprocess.run(["z3", file_path], capture_output=True, text=True)
-        return result.returncode == 0
-    except FileNotFoundError:
-        print("Z3 not found. Skipping validation.")
-        return False
+        try:
+            result = solver.solve
+            if result:
+                print("SAT with Z3.")
+                return True
+            else:
+                print("UNSAT with Z3.")
+                return True
+        except Exception as e:
+            print("Z3 solver encountered an error.")
+            print(e)
 
 
 def main():
@@ -89,9 +101,9 @@ def main():
     write_smt2_script(new_script, args.output_file)
 
     if validate_with_z3(args.output_file):
-        print("Output file is valid SMT-LIB (Z3 check passed).")
+        print("Output file is valid (Z3 check passed).")
     else:
-        print("Z3 reported an error. Output might be invalid.")
+        print("Output file is invalid (Z3 returned errors).")
 
 
 if __name__ == "__main__":
